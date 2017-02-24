356680791edc5e51baad929ddd4b5e311f6d33f6
chaerg <17710166210@163.com>
2017-02-24 16:55:44 +0800

    Add files via upload

---
+++ WQButton.h
@@ -0,0 +1,13 @@
+//
+//  WQButton.h
+//  水印
+//
+//  Created by wq on 2017/2/24.
+//  Copyright © 2017年 wangqiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WQButton : UIButton
+
+@end
---
+++ WQButton.m
@@ -0,0 +1,21 @@
+//
+//  WQButton.m
+//  水印
+//
+//  Created by wq on 2017/2/24.
+//  Copyright © 2017年 wangqiong. All rights reserved.
+//
+
+#import "WQButton.h"
+
+@implementation WQButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
